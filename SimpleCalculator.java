c5eadc428b3c4f6a32cd799e31d24af89fd2a9d3
loijoanne <joaanejinxu@gmail.com>
2024-12-27 14:53:44 +0800

    https://code.visualstudio.com/docs/sourcecontrol/github

---
+++ SimpleCalculator.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Greeting the user
+        System.out.println("Welcome to the Simple Calculator!");
+        
+        // Taking input from the user
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Choose an operation: +, -, *, /");
+        char operation = scanner.next().charAt(0);
+
+        double result = 0;
+
+        // Performing the operation
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    scanner.close();
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operation.");
+                scanner.close();
+                return;
+        }
+
+        // Displaying the result
+        System.out.println("The result is: " + result);
+
+        scanner.close();
+    }
+}
